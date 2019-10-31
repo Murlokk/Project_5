3b1dc5b981bb3d413f3c1afae3cdf14962025509
Alexa Ciprian <alexaciprian302@yahoo.ro>
2019-10-31 14:51:37 +0200

    Sublinerea unui text cand punem cursorul deasupra lui

---
+++ Sublinerea unui text cand punem cursorul pe el/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sublinerea_unui_text_cand_punem_cursorul_pe_el
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextSubliniat_MouseEnter(object sender, MouseEventArgs e)
+        {
+            TextSubliniat.TextDecorations = TextDecorations.Underline;
+        }
+
+        private void TextSubliniat_MouseLeave(object sender, MouseEventArgs e)
+        {
+            TextSubliniat.TextDecorations = null;
+        }
+    }
+}
